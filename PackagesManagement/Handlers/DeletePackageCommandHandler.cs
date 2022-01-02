fa86719d2f6cf3efc4f6b7396dc1167c94e3bc91
M F M Fazrin <mfmfazrin1986@gmail.com>
2022-01-02 16:26:11 +0530

    Add project files.

---
+++ PackagesManagement/Handlers/DeletePackageCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using DDD.ApplicationLayer;
+using DDD.DomainLayer;
+using PackagesManagement.Commands;
+using PackagesManagementDomain.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PackagesManagement.Handlers
+{
+    public class DeletePackageCommandHandler : ICommandHandler<DeletePackageCommand>
+    {
+        private IPackageRepository repo;
+        private IEventMediator mediator;
+
+        public DeletePackageCommandHandler(IPackageRepository repo, IEventMediator mediator)
+        {
+            this.repo = repo;
+            this.mediator = mediator;
+        }
+
+        public async Task HandleAsync(DeletePackageCommand command)
+        {
+            var deleted = await repo.Delete(command.PackageId);
+            if (deleted != null)
+                await mediator.TriggerEvents(deleted.DomainEvents);
+            await repo.UnitOfWork.SaveEntitiesAsync();
+        }
+    }
+}
